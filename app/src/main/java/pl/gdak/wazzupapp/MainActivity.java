5d3aee0b585f604474dfd338174c941c5f26f188
Krystian Bąkowski <krystian.bakowskiv8@wp.pl>
2018-10-01 18:20:19 +0200

    Dodanie Splash Screen i ukrycie nagłowka androida w aplikacji

--- app/src/main/java/pl/gdak/wazzupapp/MainActivity.java
+++ app/src/main/java/pl/gdak/wazzupapp/MainActivity.java
@@ -15,6 +15,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import android.widget.ImageButton;
@@ -36,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //zainicjuj baze
---
+++ app/src/main/java/pl/gdak/wazzupapp/SplashScreenActivity.java
@@ -0,0 +1,49 @@
+package pl.gdak.wazzupapp;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).hide();
+        }
+        loading_app();
+    }
+    private void loading_app() {
+        new CountDownTimer(2000, 10) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+//                progress = progress + 1;
+//                progressBar.setProgress(progress);
+            }
+
+            @Override
+            public void onFinish() {
+
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }.start();
+    }
+
+
+
+}
