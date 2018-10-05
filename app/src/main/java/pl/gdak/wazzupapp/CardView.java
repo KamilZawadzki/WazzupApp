d807463f13354ffb062a1b202763a83962980fb6
Krystian Bąkowski <krystian.bakowskiv8@wp.pl>
2018-10-05 16:37:40 +0200

    Dodanie Card view ( nie podłączone przyciski)

--- app/src/main/java/pl/gdak/wazzupapp/CardView.java
+++ app/src/main/java/pl/gdak/wazzupapp/CardView.java
@@ -1,31 +1,20 @@
 package pl.gdak.wazzupapp;
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
 
-import java.util.Objects;
-
-public class SplashScreenActivity extends AppCompatActivity {
-
-
+public class CardView extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash_screen);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Objects.requireNonNull(getSupportActionBar()).hide();
-        }
-        loading_app();
+        setContentView(R.layout.activity_card_view);
+
+
     }
     private void loading_app() {
         new CountDownTimer(2000, 10) {
@@ -39,11 +28,9 @@ public class SplashScreenActivity extends AppCompatActivity {
             public void onFinish() {
 
 
-                startActivity(new Intent(getApplicationContext(), CardView.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         }.start();
     }
-
-
 
 }
